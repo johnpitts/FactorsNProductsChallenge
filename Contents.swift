b629ea651e63fdb88c59386207a3b8380f0a8a4f
johnpitts <jgpitts@gmail.com>
2020-04-10 10:51:57 -0400

    solved Between Two Sets on HackerRank, involving finding numbers which serve as both factors for one array and products for another array

--- Contents.swift
+++ Contents.swift
@@ -1,5 +1,42 @@
 import Cocoa
 
+let row1 = [4, 8, 2]
+let row2 = [4, 5, 7]
+let row3 = [6, 1, 6]
+let matrix = [row1, row2, row3]
+
+func formingMagicSquare(s: [[Int]]) -> Int {
+    
+    var solutionMatrix: [[Int]] = s
+    
+    // check all roews to see if sum = 15; if yes, write all values to solutionMatrix
+    for eachRow in s {
+        var sum = 0
+        for eachElement in eachRow {
+            sum += eachElement
+        }
+        print(sum)
+        if sum == 15 {
+            solutionMatrix
+        }
+    }
+    
+    return 0
+}
+
+print(formingMagicSquare(s: matrix))
+
+
+
+
+
+
+
+
+
+
+
+
 // Find all the numbers in existence which are factorable by array a, but are also factors of all the elements in array b!
 
 let a = [2, 4]
